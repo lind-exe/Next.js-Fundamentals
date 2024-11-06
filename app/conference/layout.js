42307671f876d964d419ab1d1269321f7aac706d
Jonathan Lind <jojje_lind@live.se>
2024-11-06 16:19:54 +0100

    adds basic styling to universal header

--- app/conference/layout.js
+++ app/conference/layout.js
@@ -3,9 +3,6 @@ import style from "./styles.css";
 export default function ConferenceLayout({ children }) {
   return (
     <>
-      <header style={style.header}>
-        GLOBOMANTICS MANIACALLY TAKING TECH TO THE GLOBE
-      </header>
       <section>{children}</section>
     </>
   );
--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,41 @@
+import { Open_Sans } from "next/font/google";
+import Link from "next/link";
+import styles from "./rootStyle.module.css";
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  weights: [400],
+});
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={openSans.className}>
       <head />
-      <body>{children}</body>
+      <body>
+        <header className={styles.header}>
+          <h1>
+            <Link href="/home" className={styles.homeLink}>
+              Globomantics
+            </Link>
+          </h1>
+          <h3>
+            <Link href="/blog" className={styles.menuBarLink}>
+              Blog
+            </Link>
+          </h3>
+          <h3>
+            <Link href="/settings" className={styles.menuBarLink}>
+              Settings
+            </Link>
+          </h3>
+          <h3>
+            <Link href="/conference" className={styles.menuBarLink}>
+              Conference
+            </Link>
+          </h3>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
